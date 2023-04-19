af695a5c2a7f600a9bda061484b0f2f37873b727
Aaron Regier <babylaker6@gmail.com>
2023-04-19 14:02:50 -0500

    test1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // import "/node_modules/dotenv/config";
-import { connect } from "/node_modules/@planetscale/database/dist/index.js";
+import { connect } from "@planetscale/database";
 const config = {
   host: aws.connect.psdb.cloud,
   username: rg5rw4dmf3buezajhwid,
