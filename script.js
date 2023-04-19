5466bb3df08ae59dcfa776e25f97218e4c1070b7
Aaron Regier <babylaker6@gmail.com>
2023-04-19 14:01:31 -0500

    test

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 import { connect } from "/node_modules/@planetscale/database/dist/index.js";
 const config = {
   host: aws.connect.psdb.cloud,
-  username: coxbrs0khabpvnfyppgl,
-  password: pscale_pw_JWNQQUwkHCRtMWIgB2M7tlpky1C6dagJNLr4TCBl901,
+  username: rg5rw4dmf3buezajhwid,
+  password: pscale_pw_OA1d9AUflejqC5cAOxRgCa1RkRPwDl3db7dDkQEzpnn,
 };
 const conn = connect(config);
 const results = await conn.execute("SELECT * FROM users");
