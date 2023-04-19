88da38f6f65c91d66e17ed6e0fb152fcbbda7230
Aaron Regier <babylaker6@gmail.com>
2023-04-19 08:59:03 -0500

    123

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 "use strict";
 
-async function insertUser() {
-  const url = `/.netlify/functions/InsertUser`;
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-}
+// async function insertUser() {
+//   const url = `/.netlify/functions/InsertUser`;
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     return data;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
 console.log(insertUser());
