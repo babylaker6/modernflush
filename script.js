7c6bd62383e6f314577bc07cb257eac9b3f2c250
Aaron Regier <babylaker6@gmail.com>
2023-04-19 12:58:36 -0500

    cnptest

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
+import "dotenv/config";
 import { connect } from "@planetscale/database";
-
 const config = {
-  host: "aws.connect.psdb.cloud",
-  username: "jbcd1hq5v5mdf6eur741",
-  password:
-    "DATABASE_PASSWORD=pscale_pw_W2rExiPF6xzbwiMdtw3rvQLF3sfnhZJqQ5zkcU68iBr",
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
 };
-
-async function getUsers() {
-  const conn = connect(config);
-
-  const results = await conn.execute("SELECT * FROM users");
-  return results;
-}
-
-console.log(getUsers());
+const conn = connect(config);
+const results = await conn.execute("SELECT * FROM users");
