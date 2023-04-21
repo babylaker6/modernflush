7f69f62114ac53f4c697bddfb9ffb1064aa98fdb
Aaron Regier <babylaker6@gmail.com>
2023-04-21 09:38:56 -0500

    parcelBuild

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 // import "/node_modules/dotenv/config";
-import { connect } from "/node_modules/planetscale/database/dist/index.js";
+import { connect } from "@planetscale/database";
 const config = {
   host: aws.connect.psdb.cloud,
   username: rg5rw4dmf3buezajhwid,
   password: pscale_pw_OA1d9AUflejqC5cAOxRgCa1RkRPwDl3db7dDkQEzpnn,
 };
-const conn = connect(config);
-const results = await conn.execute("SELECT * FROM users");
-console.log(results);
+
+async function getUsers() {
+  const conn = connect(config);
+  const results = await conn.execute("SELECT * FROM users");
+  console.log(results);
+}
+
+getUsers();
+
+if (module.hot) {
+  module.hot.accept();
+}
