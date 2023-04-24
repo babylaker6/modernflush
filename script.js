32a4a65db6860345eee94520db7fd86e3c82d4d3
Aaron Regier <babylaker6@gmail.com>
2023-04-24 13:05:55 -0500

    undo

--- script.js
+++ script.js
@@ -7,14 +7,18 @@
 //   password: pscale_pw_OA1d9AUflejqC5cAOxRgCa1RkRPwDl3db7dDkQEzpnn,
 // };
 
-const url = `/.netlify/functions/getAllUsers`;
-try {
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-} catch (err) {
-  console.log(err);
+async function getUsersTable() {
+  const url = `/.netlify/functions/getAllUsers`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
 }
+
+getUsersTable();
 
 // if (module.hot) {
 //   module.hot.accept();
