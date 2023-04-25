c3022a21a1a1216bea2da809dd4369b4c90bc892
Aaron Regier <babylaker6@gmail.com>
2023-04-25 12:24:12 -0500

    test

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 console.log("test1");
-require("dotenv").config();
+import {} from "dotenv";
 console.log("test2");
-const mysql = require("mysql2");
+import * as mysql from "mysql2";
+// const mysql = require("mysql2");
 console.log("test3");
-const connection = mysql.createConnection(process.env.DATABASE_URL);
-console.log("Connected to PlanetScale!");
-connection.end();
+// const connection = mysql.createConnection(process.env.DATABASE_URL);
+// console.log("Connected to PlanetScale!");
+// connection.end();
