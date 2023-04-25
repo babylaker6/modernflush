cfbf9204b423bcffc3e0e4c45474f9cb4faff126
Aaron Regier <babylaker6@gmail.com>
2023-04-25 14:23:35 -0500

    reload

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 import "dotenv/config";
 import { connect } from "@planetscale/database";
 
-const config = {
-  host: "aws.connect.psdb.cloud",
-  username: "tnz4w42hd6ve6h7uxr6g",
-  password: "pscale_pw_3q9rUVLijQInGUKR07N1JCJkQVbn4fsA8OXaNJe6XRJ",
-};
-
-const conn = connect(config);
-
 async function getUsers() {
+  const config = {
+    host: "aws.connect.psdb.cloud",
+    username: "tnz4w42hd6ve6h7uxr6g",
+    password: "pscale_pw_3q9rUVLijQInGUKR07N1JCJkQVbn4fsA8OXaNJe6XRJ",
+  };
+
+  const conn = await connect(config);
+
   const results = await conn.execute("SELECT * FROM users");
   console.log("test cl");
   console.log(results);
