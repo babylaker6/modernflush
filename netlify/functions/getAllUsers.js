217848af95d715ce8ae39ebbde7578a3ca2a0918
Aaron Regier <babylaker6@gmail.com>
2023-04-25 08:43:26 -0500

    starter1

--- netlify/functions/getAllUsers.js
+++ netlify/functions/getAllUsers.js
@@ -1,13 +1,13 @@
 import "dotenv/config";
 import { connect } from "@planetscale/database";
 
-const config = {
-  host: process.env.DATABASE_HOST,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-};
-const conn = connect(config);
-
 exports.handler = async function (event, context) {
+  const config = {
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+  };
+  const conn = connect(config);
+
   const results = await conn.execute("select * from users");
 };
