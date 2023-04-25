7b2292dd3cc2647b7553ded9219c8d99cb988d6a
Aaron Regier <babylaker6@gmail.com>
2023-04-25 11:47:25 -0500

    retest

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-console.log("test cl");
-// import {} from "dotenv/config";
-const mysql = require("mysql2/index.js");
-console.log("test cl2");
-// const mysql = import("mysql2/promise");
-console.log("test cl");
+console.log("test1");
+require("dotenv").config();
+console.log("test2");
+const mysql = require("mysql2");
+console.log("test3");
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 console.log("Connected to PlanetScale!");
 connection.end();
-
-// async function getUsersTable() {
-//   const url = `/.netlify/functions/getAllUsers`;
-//   try {
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     console.log(data);
-//   } catch (err) {
-//     console.log("error");
-//   }
-// }
-
-// getUsersTable();
